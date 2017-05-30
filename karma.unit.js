38c57e1f75a0a87c46306898db464adff6954043
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-05-30 22:07:20 +0200

    Enable tests in Chrome browser in Travis CI.

--- karma.unit.js
+++ karma.unit.js
@@ -1,27 +1,39 @@
 var webpackConfig = require('./config/webpack.config.test');
 
+var configuration = {
+  basePath: '',
+  frameworks: ['mocha', 'chai', 'sinon'],
+  files: [
+    'node_modules/es6-promise/dist/es6-promise.auto.js',
+    'src/test.ts'
+  ],
+  preprocessors: {
+    'src/test.ts': ['webpack']
+  },
+  webpack: webpackConfig,
+  webpackServer: { noInfo: true },
+  reporters: ['mocha'],
+  port: 9876,
+  colors: true,
+  autoWatch: false,
+  browsers: ['Chrome'],
+  mime: {
+    'text/x-typescript': ['ts']
+  },
+  singleRun: true,
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
+    }
+  }
+};
+
+if (process.env.TRAVIS) {
+  configuration.browsers = ['Chrome_travis_ci'];
+}
+
 module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['mocha', 'chai', 'sinon'],
-    files: [
-      'node_modules/es6-promise/dist/es6-promise.auto.js',
-      'src/test.ts'
-    ],
-    preprocessors: {
-      'src/test.ts': ['webpack']
-    },
-    webpack: webpackConfig,
-    webpackServer: { noInfo: true },
-    reporters: ['mocha'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Chrome'],
-    mime: {
-      'text/x-typescript': ['ts']
-    },
-    singleRun: true
-  });
+  configuration.logLevel = config.LOG_INFO;
+  config.set(configuration);
 };
