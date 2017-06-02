a8ae65e6c037383f5ca94ca311af89952f613a8e
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-06-02 14:14:47 +0200

    Instruct Travis to run coverage tests in latest Chrome browser instead of older Chromium.

--- karma.coverage.js
+++ karma.coverage.js
@@ -18,45 +18,56 @@ if (args.env === 'jk') {
   reporters = ['junit', 'coverage'];
 }
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['mocha', 'chai', 'sinon'],
-    files: [
-      'node_modules/es6-promise/dist/es6-promise.auto.js',
-      'src/test.ts'
-    ],
-    reporters: reporters,
-    preprocessors: {
-      'src/test.ts': ['webpack']
-    },
-    webpack: {
-      devtool: 'inline-source-map',
-      resolve: webpackConfig.resolve,
-      module: webpackConfig.module
-    },
-    webpackServer: {
-      noInfo: true
-    },
-    junitReporter: {
-      outputDir: 'reports/'
-    },
-    coverageReporter: {
-      reporters: [{
-        type: 'json',
-        dir: 'coverage/json',
-        subdir: '.'
-      }]
-    },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Chrome'],
-    mime: {
-      'text/x-typescript': ['ts']
-    },
-    singleRun: true,
+var configuration = {
+  basePath: '',
+  frameworks: ['mocha', 'chai', 'sinon'],
+  files: [
+    'node_modules/es6-promise/dist/es6-promise.auto.js',
+    'src/test.ts'
+  ],
+  reporters: reporters,
+  preprocessors: {
+    'src/test.ts': ['webpack']
+  },
+  webpack: {
+    devtool: 'inline-source-map',
+    resolve: webpackConfig.resolve,
+    module: webpackConfig.module
+  },
+  webpackServer: {
+    noInfo: true
+  },
+  junitReporter: {
+    outputDir: 'reports/'
+  },
+  coverageReporter: {
+    reporters: [{
+      type: 'json',
+      dir: 'coverage/json',
+      subdir: '.'
+    }]
+  },
+  port: 9876,
+  colors: true,
+  autoWatch: false,
+  browsers: ['Chrome'],
+  mime: {
+    'text/x-typescript': ['ts']
+  },
+  singleRun: true,
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
+    }
+  }
+};
 
-  });
+if(process.env.TRAVIS) {
+  configuration.browsers = ['Chrome_travis_ci'];
+}
+
+module.exports = function(config) {
+  configuration.logLevel = config.LOG_INFO;
+  config.set(configuration);
 };
