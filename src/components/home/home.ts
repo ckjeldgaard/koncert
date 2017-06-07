046c76912c9b891d5a1782c5904e59ed1aeb6072
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-06-07 10:15:49 +0200

    Displaying events from Firebase.

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,11 +1,36 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import {db} from '../../firebase';
+
+interface Event {
+  cancelled: boolean;
+  dateStart: number;
+  dateEnd: number;
+  festival: boolean;
+  name: string;
+  venue: string;
+}
 
 @Component({
     template: require('./home.html')
 })
 export class HomeComponent extends Vue {
 
-    homeName: string = 'Koncert';
+  events: Event[] = [];
+  homeName: string = 'Koncert';
+
+  mounted() {
+    let ref = db.ref('data/events');
+    ref.on('value', (snapshot) => {
+      this.updateEvents(snapshot.val());
+    });
+  }
+
+  private updateEvents(data: any) {
+    for (let key in data) {
+      let value = data[key];
+      this.events.push(value);
+    }
+  }
 
 }
