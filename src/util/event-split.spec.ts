e98c08a042e2ab2441e3b42f038894a27b005e24
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-07-12 10:25:18 +0200

    Added unit tests for EventSplit.

--- src/util/event-split.spec.ts
+++ src/util/event-split.spec.ts
@@ -14,7 +14,8 @@ describe('Event split', () => {
       dateStart: JANUARY_2017,
       festival: true,
       name: 'Event 1',
-      venue: 'Venue 1'
+      venue: 'Venue 1',
+      province: 'province1'
     },
     event2: {
       cancelled: false,
@@ -22,7 +23,17 @@ describe('Event split', () => {
       dateStart: FEBRUARY_2017,
       festival: true,
       name: 'Event 2',
-      venue: 'Venue 2'
+      venue: 'Venue 2',
+      province: 'province2'
+    },
+    event3: {
+      cancelled: false,
+      dateEnd: JANUARY_2017,
+      dateStart: JANUARY_2017,
+      festival: true,
+      name: 'Event 3',
+      venue: 'Venue 3',
+      province: 'province1'
     }
   };
 
@@ -31,6 +42,12 @@ describe('Event split', () => {
 
     expect(eventSplit.splitByMonths()[0].name).to.equal('January 2017');
     expect(eventSplit.splitByMonths()[1].name).to.equal('February 2017');
+  });
+
+  it('should filter out provinces', async () => {
+    let eventSplit: EventSplit = new EventSplit(testEvents);
+    expect(eventSplit.splitByMonths('province1').length).to.equal(1);
+    expect(eventSplit.splitByMonths('province1')[0].events.length).to.equal(2);
   });
 
 });
