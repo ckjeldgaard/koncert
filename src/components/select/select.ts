80abe7bd6a5f0d304b4fe335481d6ddb1b2fac83
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-08-29 20:03:17 +0200

    Added vue-on-click-outside directive to close select box.

--- src/components/select/select.ts
+++ src/components/select/select.ts
@@ -1,13 +1,22 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { mixin as onClickOutside } from 'vue-on-click-outside';
 
 @Component({
-  template: require('./select.html')
+  template: require('./select.html'),
+  mixins: [onClickOutside]
 })
 export class SelectComponent extends Vue {
+
+  open: Boolean = false;
+
   mounted() {
     this.$nextTick(() => {
 
     });
+  }
+
+  close() {
+    this.open = false;
   }
 }
