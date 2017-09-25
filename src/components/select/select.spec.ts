bdb3de0d193908e651fd67138c7b8e9249f9d59e
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-09-25 16:05:28 +0200

    Added more unit tests for SelectComponent.

--- src/components/select/select.spec.ts
+++ src/components/select/select.spec.ts
@@ -2,6 +2,17 @@ import { expect } from 'chai';
 import {SelectComponent} from './select';
 import {mount, Wrapper} from 'avoriaz';
 import { spy, assert } from 'sinon';
+import {isUndefined} from "util";
+
+let props = {
+  id: 'genres',
+  options: [
+    {key: 'option1', value: 'Option 1'},
+    {key: 'option2', value: 'Option 2'}
+  ],
+  placeholder: 'Select an option',
+  multiple: false
+};
 
 describe('Select component', () => {
   it('correctly sets the placeholder text', () => {
@@ -13,18 +24,7 @@ describe('Select component', () => {
 
   it('selects a single option', () => {
     const busSpy = spy();
-    const wrapper: Wrapper = mount(
-      SelectComponent, {
-        propsData: {
-          id: 'genres',
-          options: [
-            {key: 'option1', value: 'Option 1'},
-            {key: 'option2', value: 'Option 2'}
-          ],
-          placeholder: 'Select an option',
-          multiple: false}
-      }
-    );
+    const wrapper: Wrapper = mount(SelectComponent, {propsData: props});
     wrapper.vm.$data['bus'] = {
       '$emit': busSpy
     };
@@ -35,6 +35,41 @@ describe('Select component', () => {
     expect(wrapper.find('label > span')[1].text()).to.equal('Option 1');
     assert.called(busSpy);
     assert.calledWith(busSpy, 'genres', [{key: 'option1', value: 'Option 1'}]);
+  });
+
+  it('is able to select multiple options', () => {
+    const busSpy = spy();
+    props.multiple = true;
+    const wrapper: Wrapper = mount(SelectComponent, {propsData: props});
+    wrapper.vm.$data['bus'] = {
+      '$emit': busSpy
+    };
+
+    const option1 = wrapper.find('ul > li > input')[0];
+    option1.trigger('click');
+    const option2 = wrapper.find('ul > li > input')[1];
+    option2.trigger('click');
+
+    expect(wrapper.find('label > span')[1].text()).to.equal('Option 1, Option 2');
+    assert.called(busSpy);
+    assert.calledWith(busSpy, 'genres', [{key: 'option1', value: 'Option 1'}, {key: 'option2', value: 'Option 2'}]);
+  });
+
+  it('should remove selected options when deselecting', () => {
+    const busSpy = spy();
+    props.multiple = true;
+    const wrapper: Wrapper = mount(SelectComponent, {propsData: props});
+    wrapper.vm.$data['bus'] = {
+      '$emit': busSpy
+    };
+
+    // Select first and second option:
+    wrapper.find('ul > li > input')[0].trigger('click');
+    wrapper.find('ul > li > input')[1].trigger('click');
+    // Deselect first option:
+    wrapper.find('ul > li > input')[0].trigger('click');
+
+    expect(wrapper.find('label > span')[1].text()).to.equal('Option 2');
   });
 
 });
