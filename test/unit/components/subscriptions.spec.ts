f0112506e1a5aad3e16bb5a153af54c12a09c98d
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-09-26 16:10:04 +0200

    Test selection of artist.

--- test/unit/components/subscriptions.spec.ts
+++ test/unit/components/subscriptions.spec.ts
@@ -21,4 +21,20 @@ describe('Subscriptions component', () => {
     expect(wrapper.vm.$data['artistsSearchResult']).to.equal(MockServiceApi.testArtists);
     expect(wrapper.find('.search-input > ul')[0].html()).to.equal('<ul><li>Alice</li><li>Bob</li></ul>');
   });
+
+  it('should select an artist', async () => {
+    const wrapper: Wrapper = mount(SubscriptionsComponent);
+    setServiceApi(wrapper, spy());
+
+    let searchInput: Wrapper = wrapper.find('input')[0];
+    (<HTMLInputElement>searchInput.element).value = 'searchArtist';
+    searchInput.trigger('input');
+
+    // Select first artist option in dropdown:
+    const option = wrapper.find('.search-input > ul > li')[0];
+    option.trigger('click');
+
+    expect(wrapper.vm.$data['buttonDisabled']).to.be.false;
+    expect((<HTMLInputElement>wrapper.find('input')[0].element).value).to.equal('Alice');
+  });
 });
