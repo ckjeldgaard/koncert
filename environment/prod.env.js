973cc2957b7528694806e20b4bb8d31f62e6a084
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-10-11 15:04:15 +0200

    Push API is served over HTTPS in production.

--- environment/prod.env.js
+++ environment/prod.env.js
@@ -10,5 +10,5 @@ module.exports = {
     storageBucket: '"koncert-9a232.appspot.com"',
     messagingSenderId: '"664870108843"'
   },
-  PUSH_API_URL: '"http://heavymetal.dk/"'
+  PUSH_API_URL: '"https://heavymetal.dk/"'
 }
