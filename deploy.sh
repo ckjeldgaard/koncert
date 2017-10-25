3f937d05cff44dcba6d9e63c2d8022bcff452e07
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-10-25 12:57:05 +0200

    Added cypress integration tests.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 set -ev
 
 npm run lint
+npm run mock
+npm run e2e
 npm run build
 npm run coverage:run
 npm run codecov
---
+++ test/integration/concerts.spec.js
@@ -0,0 +1,29 @@
+describe('Concerts page', function() {
+  it('Should search for a specific concert', function() {
+    cy.visit('/');
+
+    cy.get('input[type=text]')
+      .type('Concert3').should('have.value', 'Concert3');
+
+    cy.get('.card label .concert-title')
+      .should('have.length', 1);
+  });
+
+  it('Should filter by province', function() {
+    cy.visit('/');
+
+    cy.get('.provinces .select .card label')
+      .first()
+      .click();
+
+    cy.get('.provinces .select .card ul li')
+      .should('have.length', 4);
+
+    cy.get('.provinces .select .card ul li')
+      .last()
+      .click();
+
+    cy.get('.card label .concert-title')
+      .should('have.length', 3);
+  });
+});
