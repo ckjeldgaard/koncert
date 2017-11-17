8945406cde923f95e3d9d13616471d3812c72438
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-11-17 10:59:46 +0100

    #1 Cleaned up region.spec.ts tests.

--- test/unit/util/region.spec.ts
+++ test/unit/util/region.spec.ts
@@ -4,20 +4,24 @@ import {Region} from '../../../src/util/region/region';
 
 describe('Region', () => {
 
-  it ('should check if coordinate is inside a region', () => {
-    const userPosition: Point = new Position(55.684628399999994, 12.5861179);
+  let region: Region;
+  beforeEach(() => {
+    let square: Point[] = [];
+    square.push(new Position(0, 0));
+    square.push(new Position(0, 2));
+    square.push(new Position(2, 2));
+    square.push(new Position(2, 0));
+    region = new Region(square);
+  });
 
-    let coords: Point[] = [];
-    coords.push(new Position(56.1639061, 12.2332764));
-    coords.push(new Position(55.8290587, 13.2934570));
-    coords.push(new Position(55.1003733, 12.7166748));
-    coords.push(new Position(54.5178931, 12.0684814));
-    coords.push(new Position(54.6325179, 10.9039307));
-    coords.push(new Position(55.4227790, 11.0028076));
-    coords.push(new Position(55.8999561, 10.7171631));
-    const zealand: Region = new Region(coords);
+  it ('should check if point is inside a region', () => {
+    const position: Point = new Position(1, 1);
+    expect(position.inside(region)).toBeTruthy();
+  });
 
-    expect(userPosition.inside(zealand)).toBeTruthy();
+  it ('should check if point is outside a region', () => {
+    const position: Point = new Position(3, 3);
+    expect(position.inside(region)).toBeFalsy();
   });
 
 });
